4608d7a33b607d2b04062eb748fe6a6537a6513c
Christophe Varoqui <christophe.varoqui@opensvc.com>
2023-02-06 17:55:13 +0100

    Implement the recursor cache wiping on ZoneRecord events
    Support v2 janitor OPENSVC_LSNR_SOCK and OPENSVC_RECURSOR_SOCK env
    variables to ease migration.
    
    Implements reconnects on both side.

--- main.go
+++ main.go
@@ -2,78 +2,245 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"sync/atomic"
 	"time"
 
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"opensvc.com/opensvc/core/client"
 	"opensvc.com/opensvc/core/event"
-	"opensvc.com/opensvc/daemon/msgbus"
+)
+
+type (
+	zoneRecordEvent struct {
+		Path string
+		Node string
+		Name string
+	}
 )
 
 var (
-	apiUrl = "/var/lib/opensvc/lsnr/h2.sock"
+	lastGetEventReader time.Time
+	lastDial           time.Time
+	osvcSock           string
+	pdnsSock           string
+	tempSock           string
+	connectInterval    time.Duration
+
+	pdnsIsConnected   atomic.Bool
+	pdnsConn          net.Conn
+	evHandlingTimeout = 300 * time.Millisecond
+	logLevel          string
+
+	defaultOSVCSock = "/var/lib/opensvc/lsnr/h2.sock"
+	defaultPDNSSock = "/var/run/pdns-recursor/pdns_recursor.controlsocket"
+	defaultLogLevel = "info"
 )
 
 func main() {
-	delay := 2 * time.Second
-	for {
-		if err := watch(); err != nil {
-			fmt.Printf("watch error %s\n", err)
-		}
-		fmt.Printf("wait %s before retry...\n", delay)
-		time.Sleep(delay)
+	if s, ok := os.LookupEnv("OPENSVC_LSNR_SOCK"); ok {
+		defaultOSVCSock = s
 	}
-}
+	if s, ok := os.LookupEnv("OPENSVC_RECURSOR_SOCK"); ok {
+		defaultPDNSSock = s
+	}
 
-func watch() (err error) {
-	var (
-		cli      *client.T
-		evReader event.ReadCloser
-	)
+	flag.StringVar(&osvcSock, "osvc-sock", defaultOSVCSock, "the unix domain socket of the opensvc agent api")
+	flag.StringVar(&pdnsSock, "pdns-sock", defaultPDNSSock, "the unix domain socket of the power dns recursor api")
+	flag.StringVar(&logLevel, "log", defaultLogLevel, "the log level (debug, info, warn, error)")
+	flag.DurationVar(&connectInterval, "connect-interval", 2*time.Second, "the interval between socket reconnects")
+	flag.Parse()
 
-	if cli, err = client.New(client.WithURL(apiUrl), client.WithInsecureSkipVerify(true)); err != nil {
-		return errors.Wrap(err, "create client")
+	log.Logger = log.Output(zerolog.NewConsoleWriter())
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+	switch logLevel {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
 	}
-	if evReader, err = cli.NewGetEvents().SetFilters([]string{"InstanceStatusUpdated"}).GetReader(); err != nil {
-		return errors.Wrap(err, "GetReader")
-	}
+
 	defer func() {
-		_ = evReader.Close()
+		_ = pdnsConn.Close()
+		_ = os.Remove(tempSock)
 	}()
 
-	err = watchEvents(evReader)
-	return errors.Wrap(err, "watchEvents")
-}
-
-func watchEvents(evReader event.ReadCloser) (err error) {
-	var (
-		ev *event.Event
-	)
-
 	for {
-		if ev, err = evReader.Read(); err != nil {
-			return err
+		if err := watch(); err != nil {
+			log.Error().Err(err).Msg("watch")
 		}
-		if err = showEvent(ev); err != nil {
-			return err
-		}
+		time.Sleep(connectInterval)
 	}
 }
 
-func showEvent(ev *event.Event) error {
-	fmt.Printf("------ read event kind: %s, time: %s\n", ev.Kind, ev.Time)
-	if ev.Kind != "InstanceStatusUpdated" {
-		return errors.Errorf("unexpected kind: %s", ev.Kind)
+func pdnsRedial() error {
+	minDialTime := lastDial.Add(connectInterval)
+	if time.Now().Before(minDialTime) {
+		time.Sleep(minDialTime.Sub(time.Now()))
 	}
-	var evData msgbus.InstanceStatusUpdated
-	err := json.Unmarshal(ev.Data, &evData)
-	if err != nil {
-		return errors.Errorf("Unmarshal error %s on '%s'", err, ev.Data)
+	lastDial = time.Now()
+	if pdnsConn != nil {
+		if err := pdnsConn.Close(); err != nil {
+			log.Error().Err(err).Msg("redial: pdns connection close")
+		}
+		pdnsConn = nil
 	}
-	resources := evData.Value.Resources
-	for i, r := range resources {
-		fmt.Printf("path: %s index: %d rid:%s status: %s\n", evData.Path, i, r.Rid, r.Status)
+	if tempSock != "" {
+		if err := os.Remove(tempSock); errors.Is(err, os.ErrExist) {
+			// pass
+		} else if err != nil {
+			log.Error().Err(err).Msg("redial: pdns client socket file remove")
+		}
+	}
+	if err := pdnsDial(); err != nil {
+		return err
 	}
 	return nil
+}
+
+func pdnsDial() error {
+	if tempSock == "" {
+		tempSockDir := filepath.Dir(pdnsSock)
+		if f, err := os.CreateTemp(tempSockDir, filepath.Base(pdnsSock)+".cli."); err != nil {
+			return err
+		} else {
+			tempSock = f.Name()
+			f.Close()
+			os.Remove(tempSock)
+		}
+		log.Info().Msgf("client socket created: %s => %s", tempSock, pdnsSock)
+	}
+	laddr := net.UnixAddr{tempSock, "unixgram"}
+	raddr := net.UnixAddr{pdnsSock, "unixgram"}
+	if conn, err := net.DialUnix("unixgram", &laddr, &raddr); err != nil {
+		return err
+	} else {
+		log.Info().Msg("pdns recursor connected")
+		pdnsConn = conn
+		pdnsIsConnected.Store(true)
+	}
+	return nil
+}
+
+func reGetEventReader() (event.ReadCloser, error) {
+	minGetEventReaderTime := lastGetEventReader.Add(connectInterval)
+	if time.Now().Before(minGetEventReaderTime) {
+		time.Sleep(minGetEventReaderTime.Sub(time.Now()))
+	}
+	lastGetEventReader = time.Now()
+	return getEventReader()
+}
+
+func getEventReader() (event.ReadCloser, error) {
+	cli, err := client.New(
+		client.WithURL(osvcSock),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "new client")
+	}
+	evReader, err := cli.NewGetEvents().SetFilters([]string{"ZoneRecordUpdated", "ZoneRecordDeleted"}).GetReader()
+	if err != nil {
+		return nil, errors.Wrap(err, "new event reader")
+	}
+	return evReader, nil
+}
+
+func watch() error {
+	var (
+		ev     *event.Event
+		evData zoneRecordEvent
+	)
+
+	evReader, err := getEventReader()
+	if err != nil {
+		return err
+	}
+
+	q := make(chan zoneRecordEvent, 1)
+
+	go func() {
+		for {
+			if evReader != nil {
+				ev, err = evReader.Read()
+			}
+			if evReader == nil || ev == nil || err != nil {
+				log.Error().Err(err).Msg("reader")
+				if evReader != nil {
+					_ = evReader.Close()
+				}
+				evReader, err = reGetEventReader()
+				if err != nil {
+					log.Error().Err(err).Msg("reader: re-get event reader")
+				}
+				continue
+			}
+			if err := json.Unmarshal(ev.Data, &evData); err != nil {
+				log.Error().Err(err).Msgf("reader: unmarshal error %s on '%s'", err, ev.Data)
+				continue
+			}
+			q <- evData
+		}
+	}()
+	for {
+		ev := <-q
+		if err = onEvent(ev); err != nil {
+			log.Error().Err(err).Msg("on event")
+		}
+	}
+	if err := evReader.Close(); err != nil {
+		log.Error().Err(err).Msg("close event reader")
+	}
+	return nil
+}
+
+func onEvent(evData zoneRecordEvent) error {
+	msg := fmt.Sprintf("wipe-cache %s", evData.Name)
+	wipe := func() error {
+		deadline := time.Now().Add(evHandlingTimeout)
+		if err := pdnsConn.SetDeadline(deadline); err != nil {
+			return err
+		}
+		log.Info().Msgf(">>> %s", msg)
+		if _, err := pdnsConn.Write([]byte(msg + "\n")); err != nil {
+			return err
+		}
+		var buff [1024]byte
+		n, err := pdnsConn.Read(buff[:])
+		if err != nil {
+			return err
+		}
+		if n > 0 {
+			log.Info().Msgf("<<< %s", string(buff[:n-1]))
+		}
+		return nil
+	}
+	wiper := func() error {
+		for {
+			if !pdnsIsConnected.Load() {
+				if err := pdnsRedial(); err != nil {
+					return err
+				}
+			}
+			err := wipe()
+			switch {
+			case errors.Is(err, os.ErrDeadlineExceeded):
+				log.Error().Err(err).Msg("pdns control socket")
+			case err != nil:
+				pdnsIsConnected.Store(false)
+			default:
+				return nil
+			}
+		}
+	}
+	return wiper()
 }
